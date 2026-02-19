341947aaf8d09313b3119da2f65e821f3dd7e79c
MADHUPRATHIKA <madhuprathika24@gmail.com>
2026-02-19 10:23:52 +0530

    Time: 4 ms (25.47%) | Memory: 8.1 MB (45.65%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.c
@@ -0,0 +1,16 @@
+#include <stdbool.h>
+bool isPalindrome(int x) {
+     if (x < 0)   
+        return false;
+     int num=x;   
+     long long reverse=0;
+	while(x>0) {
+		int y=x%10;
+		reverse=reverse*10+y;
+		x=x/10;
+	}
+	if(num==reverse)
+		return true;
+	else
+		return false;
+}
