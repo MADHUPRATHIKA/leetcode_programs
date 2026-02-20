ef4b693cad777dddf167bb8a723d3c06f6ac57cb
MADHUPRATHIKA <madhuprathika24@gmail.com>
2026-02-20 09:20:13 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (76.53%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.c
@@ -0,0 +1,17 @@
+bool isValid(char* s){
+    int n=strlen(s),top=-1;
+    char stack[n];
+    for(int i=0;i<n;i++){
+        char c=s[i];
+        if(c=='('||c=='{'||c=='[')
+          stack[++top]=c;
+        else if(top==-1) 
+          return false;
+        else if((c==')'&&stack[top]!='(')||(c=='}'&&stack[top]!='{')||(c==']'&&stack[top]!='[')) 
+          return false;
+        else
+          top--;
+    }
+    return
+      top==-1;
+}
